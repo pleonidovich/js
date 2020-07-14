f70e7a14d4bfc01ffd796bf281df84b356c426a6
Pavel_Tereshko <pleonidovich11@gmail.com>
2020-07-14 13:21:14 +0300

    Initial commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src='https://upload.wikimedia.org/wikipedia/en/thumb/2/27/Arizona_Coyotes.svg/1200px-Arizona_Coyotes.svg.png'/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <img
+                src='https://avatars.mds.yandex.net/get-pdb/2821569/a27f1cf3-e9f6-4b5d-b646-e5896a4cac0c/s1200'></img>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My post
+                <div>
+                    New post
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
